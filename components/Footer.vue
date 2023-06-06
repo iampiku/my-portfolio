a684b06114404e22f737a0ac71618a1dc4e9b212
iampiku <iampikuchatterjee@gmail.com>
2023-06-06 10:21:06 +0530

    🔧 implemented footer and sidebar

---
+++ components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+	<footer class="fixed inset-x-0 bottom-0 text-center border-t-2">
+		<header class="py-2">Made with ❤️ and Nuxtjs</header>
+		<ul class="flex justify-center">
+			<li class="hover:bg-slate-300 rounded-xl p-2">
+				<img
+					src="@/assets/images/github.svg"
+					width="24"
+					height="24"
+					alt="github-img"
+				/>
+			</li>
+			<li class="hover:bg-slate-300 rounded-xl p-2">
+				<img
+					src="@/assets/images/linkedin.svg"
+					width="24"
+					height="24"
+					alt="github-img"
+				/>
+			</li>
+			<li class="hover:bg-slate-300 rounded-xl p-2">
+				<img
+					src="@/assets/images/twitter.svg"
+					width="24"
+					height="24"
+					alt="github-img"
+				/>
+			</li>
+			<li class="hover:bg-slate-300 rounded-xl p-2">
+				<a href="https://www.facebook.com/pradipta.chatterjee.75457/">
+					<img
+						src="@/assets/images/facebook.svg"
+						width="24"
+						height="24"
+						alt="github-img"
+					/>
+				</a>
+			</li>
+		</ul>
+		<p class="py-4 text-xs">&copy; 2023 copyright all right reserved</p>
+	</footer>
+</template>
---
+++ components/Sidebar.vue
@@ -0,0 +1,71 @@
+<template>
+	<nav
+		class="w-20 bg-slate-100 min-h-screen fixed flex flex-col justify-between z-10 border-r-2 border-slate-200"
+	>
+		<section>
+			<div
+				v-for="({ menuName, icon }, index) in menuItems"
+				:key="index"
+				class="my-6 py-1 mx-1 text-center text-xs hover:bg-slate-300 rounded-lg cursor-pointer"
+			>
+				<img
+					:src="icon"
+					alt="menu-icon"
+					width="16"
+					height="16"
+					class="mx-auto py-2"
+				/>
+				<span>{{ menuName }}</span>
+			</div>
+		</section>
+		<section class="pb-2 mx-auto">
+			<button
+				type="button"
+				@click="toggleTheme"
+				class="px-4 py-2 mb-2 bg-lime-100 rounded-xl hover:bg-lime-300"
+			>
+				<img
+					src="@/assets/images/settings.svg"
+					alt="theme-icon"
+					width="20"
+					height="20"
+				/>
+			</button>
+		</section>
+	</nav>
+</template>
+
+<script setup lang="ts">
+import MenuItem from "@/interfaces/MenuItem";
+
+/**
+ * Menu icons
+ */
+import HomeIcon from "@/assets/images/home.svg";
+import AboutIcon from "@/assets/images/user.svg";
+import ContactIcon from "@/assets/images/envelope.svg";
+import WorkIcon from "@/assets/images/briefcase.svg";
+
+const menuItems = useState<MenuItem[]>(() => [
+	{
+		menuName: "Home",
+		icon: HomeIcon,
+	},
+	{
+		menuName: "About",
+		icon: AboutIcon,
+	},
+	{
+		menuName: "Work",
+		icon: WorkIcon,
+	},
+	{
+		menuName: "Contact",
+		icon: ContactIcon,
+	},
+]);
+
+function toggleTheme(): void {
+	console.log("THEME Toggled");
+}
+</script>
